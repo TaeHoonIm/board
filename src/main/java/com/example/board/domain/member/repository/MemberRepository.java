85ef1ca4015b3b64659391613bc2cb873b969526
TaeHoonIm <xogns9647@naver.com>
2023-11-18 16:51:53 +0900

    회원 repository 생성
    #3 회원 repository 생성

---
+++ src/main/java/com/example/board/domain/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.board.domain.member.repository;
+
+import com.example.board.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
